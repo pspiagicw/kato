e6bb18b669c94ba805b9038144265e086dbec318
pspiagicw <pspiagicw@gmail.com>
2024-08-11 23:59:20 +0530

    devops: moved to flake.nix for including 'vhs' and wrote tapes

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "CLI configuration with vhs";
+
+  inputs = {
+    # Nixpkgs repository
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    devShell.x86_64-linux = let
+      pkgs = import nixpkgs {system = "x86_64-linux";};
+    in
+      pkgs.mkShell {
+        buildInputs = [
+          pkgs.vhs
+          pkgs.gopls
+          pkgs.bashInteractive
+        ];
+      };
+  };
+}
